3686e79e6a84f42b8c99666af58ca05b0c63ddc4
LucasGMullerS <lucasmulleryt@gmail.com>
2026-05-10 22:02:54 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -14,7 +14,7 @@ async function gerarCodigo() {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
-                "Authorization": "Bearer (sua chave aqui)"
+                "Authorization": "Bearer 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"
             },
             body: JSON.stringify({
                 "model": "mistralai/mistral-7b-instruct",
